6babf2fa227130ab67d3e6ee257f425043261181
Dratale <nvidiadratale@gmail.com>
2025-02-27 16:44:20 +0100

    First Commit + Premier test : Recherche de livre par ISBN

---
+++ src/test/java/org/tests/LibraryServiceTest.java
@@ -0,0 +1,33 @@
+package org.tests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
+
+class LibraryServiceTest {
+    private DBService dbService;
+    private WebService webService;
+    private LibraryService libraryService;
+
+    @BeforeEach
+    void setUp() {
+        dbService = Mockito.mock(DBService.class);
+        webService = Mockito.mock(WebService.class);
+        libraryService = new LibraryService(dbService, webService);
+    }
+
+    @Test
+    void givenBookIsInLocalDB_whenFindByIsbn_thenReturnBookFromLocalDB() {
+        Book expectedBook = new Book("9782266332439", "Âmes animales", "José Rodrigues Dos Santos", "Pocket", BookType.BD);
+        when(dbService.getBookByIsbn("9782266332439")).thenReturn(expectedBook);
+        when(webService.getBookByIsbn("9782266332439")).thenReturn(expectedBook);
+
+        Book book = libraryService.findBookByIsbn("9782266332439");
+
+        verifyNoInteractions(webService);  
+        assertEquals(expectedBook, book);
+    }
+}
