cb604b4162c9422fb3c2bbbcc89b0e9a59f7fee7
Dratale <nvidiadratale@gmail.com>
2025-02-28 12:09:50 +0100

    Test passé

--- src/main/java/org/exemple/Services/ReservationService.java
+++ src/main/java/org/exemple/Services/ReservationService.java
@@ -2,6 +2,7 @@ package org.exemple.Services;
 
 import java.util.ArrayList;
 
+import org.exemple.Exceptions.BookNotAvailableException;
 import org.exemple.Models.Adherant;
 import org.exemple.Models.Book;
 import org.exemple.Models.Reservation;
@@ -13,11 +14,12 @@ public class ReservationService {
         listeReservations = new ArrayList<Reservation>();
     }
 
-    
-
-    public int creerReservation(Adherant _adherant, Book _book) {
-        _book.setIsAvailable(false);
-        listeReservations.add(new Reservation(_adherant, _book));
-        return listeReservations.size();
+    public int creerReservation(Adherant _adherant, Book _book) throws Exception {
+        if(_book.getIsAvailable()==true) {
+            _book.setIsAvailable(false);
+            listeReservations.add(new Reservation(_adherant, _book));
+            return listeReservations.size();
+        }
+        else  { throw new BookNotAvailableException(""); }
     }
 }
--- src/test/java/org/tests/ReservationServiceTest.java
+++ src/test/java/org/tests/ReservationServiceTest.java
@@ -3,12 +3,12 @@ package org.tests;
 import java.time.LocalDate;
 
 import org.exemple.Enums.BookType;
+import org.exemple.Exceptions.BookNotAvailableException;
 import org.exemple.Models.Adherant;
 import org.exemple.Models.Book;
 import org.exemple.Services.ReservationService;
 import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -20,7 +20,7 @@ public class ReservationServiceTest {
     }
 
     @Test
-    void testCreerReservationAvecLivreDisponible() {
+    void testCreerReservationAvecLivreDisponible() throws Exception {
         Adherant adherant = new Adherant(1, "Vigner", "Anthony", LocalDate.of(1990, 4, 1), "Mr");
         Book book = new Book("9782266332439", "Âmes animales", "José Rodrigues Dos Santos", "Pocket", BookType.BD);
         reservationService.creerReservation(adherant, book);
