ccac7e99ad08ef3ddba61f419a5ae46422f72e6d
Dratale <nvidiadratale@gmail.com>
2025-02-28 10:18:11 +0100

    Test passé

--- src/main/java/org/exemple/classes/IsbnValidator.java
+++ src/main/java/org/exemple/classes/IsbnValidator.java
@@ -2,6 +2,23 @@ package org.exemple.classes;
 
 public class IsbnValidator {
     public boolean validateIsbn(String ISBN) {
+        if (ISBN.length()==10) {
+            return validateIsbn10(ISBN);
+        } else if (ISBN.length()==13) {
+            return validateIsbn13(ISBN);
+        }
+        return false;
+    }
+
+    private boolean validateIsbn13(String ISBN) {
+        int total=0;
+        for (int i=0; i<13;i++) {
+            total += Character.getNumericValue(ISBN.charAt(i))* (i%2==0 ? 1 : 3);
+        }
+        return total % 10 == 0;
+    }
+
+    private boolean validateIsbn10(String ISBN) {
         int total=0;
         for (int i=0; i<10;i++) {
             total += Character.getNumericValue(ISBN.charAt(i)) * (10 - i);
