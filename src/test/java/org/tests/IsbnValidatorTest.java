8872f8c217656d98850446f5e50dd0b4ad8b3366
Dratale <nvidiadratale@gmail.com>
2025-02-28 10:01:08 +0100

    Tests : Validité ISBN 13 caractères

--- src/test/java/org/tests/IsbnValidatorTest.java
+++ src/test/java/org/tests/IsbnValidatorTest.java
@@ -1,8 +1,8 @@
 package org.tests;
 
+import org.exemple.classes.IsbnValidator;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
 
 public class IsbnValidatorTest {
@@ -21,6 +21,23 @@ public class IsbnValidatorTest {
 
         boolean result = validator.validateIsbn("2213731387");
 
+        assertFalse(result);
+    }
+
+    @Test
+    public void givenISBN13characters_whenIsValid_ShouldReturnTrue() {
+        IsbnValidator validator = new IsbnValidator();
+
+        boolean result = validator.validateIsbn("9782266332439");
+
+        assertTrue(result);
+    }
+
+    @Test
+    public void givenISBN13characters_whenIsInvalid_ShouldReturnFalse() {
+        IsbnValidator validator = new IsbnValidator();
+
+        boolean result = validator.validateIsbn("9782266332437");
         assertFalse(result);
     }
 }
