3b0f9dd1168ee2b4bec1b51b64b56178d073bf7a
Dratale <nvidiadratale@gmail.com>
2025-02-28 12:00:54 +0100

    Modification du test car erreur

--- src/test/java/org/tests/ReservationServiceTest.java
+++ src/test/java/org/tests/ReservationServiceTest.java
@@ -1,11 +1,12 @@
 package org.tests;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-
 import java.time.LocalDate;
 
 import org.exemple.Enums.BookType;
+import org.exemple.Models.Adherant;
 import org.exemple.Models.Book;
+import org.exemple.Services.ReservationService;
+import static org.junit.Assert.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -14,14 +15,13 @@ public class ReservationServiceTest {
     @BeforeEach
     void setUp() {
         reservationService = new ReservationService();
-
     }
 
     @Test
     void testCreerReservationAvecLivreDisponible() {
         Adherant adherant = new Adherant(1, "Vigner", "Anthony", LocalDate.of(1990, 4, 1), "Mr");
         Book book = new Book("9782266332439", "Âmes animales", "José Rodrigues Dos Santos", "Pocket", BookType.BD);
-        int CodeReservation = reservationService.creerReservation(adherant, book);
-        assertFalse(book.getIsAvailable()); 
+        reservationService.creerReservation(adherant, book);
+        assertTrue(book.getIsAvailable()); 
     }
 }
