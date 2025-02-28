e1480a19b5de89329979b687f0e4f8aa8d569d4b
Dratale <nvidiadratale@gmail.com>
2025-02-28 10:21:00 +0100

    Test : taille ISBN incorrecte

--- src/test/java/org/tests/IsbnValidatorTest.java
+++ src/test/java/org/tests/IsbnValidatorTest.java
@@ -2,12 +2,13 @@ package org.tests;
 
 import org.exemple.classes.IsbnValidator;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 public class IsbnValidatorTest {
     @Test
-    public void givenISBN10characters_whenIsValid_ShouldReturnTrue() {
+    public void givenISBN10characters_whenIsValid_ShouldReturnTrue() throws Exception {
         IsbnValidator validator = new IsbnValidator();
 
         boolean result = validator.validateIsbn("2213731381");
@@ -16,7 +17,7 @@ public class IsbnValidatorTest {
     }
 
     @Test
-    public void givenISBN10characters_whenIsInvalid_ShouldReturnFalse() {
+    public void givenISBN10characters_whenIsInvalid_ShouldReturnFalse() throws Exception {
         IsbnValidator validator = new IsbnValidator();
 
         boolean result = validator.validateIsbn("2213731387");
@@ -25,7 +26,7 @@ public class IsbnValidatorTest {
     }
 
     @Test
-    public void givenISBN13characters_whenIsValid_ShouldReturnTrue() {
+    public void givenISBN13characters_whenIsValid_ShouldReturnTrue() throws Exception  {
         IsbnValidator validator = new IsbnValidator();
 
         boolean result = validator.validateIsbn("9782266332439");
@@ -34,10 +35,16 @@ public class IsbnValidatorTest {
     }
 
     @Test
-    public void givenISBN13characters_whenIsInvalid_ShouldReturnFalse() {
+    public void givenISBN13characters_whenIsInvalid_ShouldReturnFalse() throws Exception {
         IsbnValidator validator = new IsbnValidator();
 
         boolean result = validator.validateIsbn("9782266332437");
         assertFalse(result);
+    }
+
+    @Test
+    public void whenWrongLength_ShouldReturnFalse() throws Exception {
+        IsbnValidator validator = new IsbnValidator();
+        assertThrows(IncorrectISBNSizeException.class, () -> validator.validateIsbn("22137313877"));
     }
 }
